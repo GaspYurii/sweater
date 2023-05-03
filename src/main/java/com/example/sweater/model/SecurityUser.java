a6cb012441625ebd4074b3158f3f30c65b58f3d5
Iurii Gasparov <Iurii_Gasparov@epam.com>
2023-05-03 21:50:31 +0300

    Some fixes in User and security

--- src/main/java/com/example/sweater/model/SecurityUser.java
+++ src/main/java/com/example/sweater/model/SecurityUser.java
@@ -1,14 +1,17 @@
 package com.example.sweater.model;
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serial;
 import java.util.Collection;
 
 public class SecurityUser implements UserDetails {
 
-    private final User user;
+    @Serial
+    private static final long serialVersionUID = 8514957138524205734L;
+
+    private final transient User user;
 
     public SecurityUser(User user) {
         this.user = user;
--- src/main/java/com/example/sweater/model/User.java
+++ src/main/java/com/example/sweater/model/User.java
@@ -2,17 +2,15 @@ package com.example.sweater.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
 @Entity
 @Table(name = "users")
-public class User implements UserDetails {
+public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -45,29 +43,5 @@ public class User implements UserDetails {
         roles.add(role);
     }
 
-    // security
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles;
-    }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return active;
-    }
 }
